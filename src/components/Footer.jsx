9b53934a7b5338ef943907ad87f5329dc4033402
elisee9 <ketounouelisee9@gmail.com>
2025-06-16 05:35:55 +0100

    Le build final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -54,7 +54,7 @@ function Footer() {
                </p>
             </motion.div>
 
-            {/*  Liens */}
+            {/*  Liens  */}
             <motion.div
                initial={{ opacity: 0, y: 20 }}
                whileInView={{ opacity: 1, y: 0 }}
