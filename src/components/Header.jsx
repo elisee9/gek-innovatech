7f5fca9ee904fc826891e4b40f56aeaaa68bdd84
elisee9 <ketounouelisee9@gmail.com>
2025-06-18 06:04:10 +0100

    Correctiion du header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -72,7 +72,7 @@ const Header = () => {
                   href="https://wa.me/22965426510"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="bg-[#7F00FF] px-5 py-2 rounded-full font-medium text-white hover:bg-[#6b00d8] transition"
+                  className="bg-[#00D2A8] hover:bg-[#00b795] px-5 py-2 rounded-full font-medium text-white transition"
                >
                   WhatsApp
                </a>
